b454b36d912be0a5995c8fb28ca1884b17b1a676
Jefferson Guanabara <jefferson.guanabara@dcx.ufpb.br>
2024-04-03 16:40:52 -0300

    first lesson nlw unite react-native trail

---
+++ src/app/register.tsx
@@ -0,0 +1,47 @@
+import { Input } from "@/components/input"
+import { View, Image, StatusBar } from "react-native"
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
+import { colors } from "@/styles/colors"
+import { Button } from "@/components/button"
+import { Link } from "expo-router"
+
+export default function Register() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content"/>
+      <Image 
+        source={require("@/assets/logo.png")} 
+        className="h-16" 
+        resizeMode="contain" 
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <FontAwesome6 
+            name="user-circle" 
+            color={colors.green[200]} 
+            size={20} 
+          />
+          <Input.Field placeholder="Nome Completo"/>
+        </Input>
+
+        <Input>
+          <MaterialIcons 
+            name="alternate-email" 
+            color={colors.green[200]} 
+            size={20} 
+          />
+          <Input.Field placeholder="E-mail" keyboardType="email-address"/>
+        </Input>
+
+        <Button title="Realizar inscrição" />
+        
+        <Link 
+          href="/" 
+          className="text-gray-100 text-base font-bold text-center mt-8"
+        >
+          Já possui ingresso?
+        </Link>
+      </View>
+    </View>
+  )
+}
